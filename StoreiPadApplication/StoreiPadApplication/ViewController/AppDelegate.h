a11635dd862207502a85236ef878d4391abc8194
hongdi@rrkd.cn <hongdi@rrkd.cn>
2016-08-22 16:49:34 +0800

    Basic components added

---
+++ StoreiPadApplication/StoreiPadApplication/ViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreiPadApplication
+//
+//  Created by HongDi Huang on 8/22/16.
+//  Copyright © 2016 HongDi Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreiPadApplication/StoreiPadApplication/ViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreiPadApplication
+//
+//  Created by HongDi Huang on 8/22/16.
+//  Copyright © 2016 HongDi Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
